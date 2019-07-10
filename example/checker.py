71a443144fd0fc354e9570ec5efcaf78bdf216a5
Benedikt Radtke <benediktradtke@gmail.com>
2019-07-10 22:25:25 +0200

    Pass a mongodb collection to the handlers

--- example/checker.py
+++ example/checker.py
@@ -6,6 +6,7 @@ import aiohttp
 
 from enochecker_async import BaseChecker, BrokenServiceException, create_app, OfflineException, ELKFormatter, CheckerTaskMessage
 from logging import LoggerAdapter
+from motor import MotorCollection
 
 class TestChecker(BaseChecker):
     port = 9012
@@ -13,36 +14,35 @@ class TestChecker(BaseChecker):
     def __init__(self):
         super(TestChecker, self).__init__("TestService", 8080)
 
-    async def test(self, logger: LoggerAdapter):
-        async with aiohttp.ClientSession() as session:
-            async with session.get("http://enowars.com") as response:
-                assert response.status == 200
-                ret = await response.read()
-                print(ret)
+    async def test(self, logger: LoggerAdapter, collection: MotorCollection):
+        document = { 'key': 'value'}
+        result = await collection.insert_one(document)
+        logger.info('result %s' % repr(result.inserted_id))
 
-    async def putflag(self, logger: LoggerAdapter, task: CheckerTaskMessage) -> None:
-        await self.test(logger)
+    async def putflag(self, logger: LoggerAdapter, task: CheckerTaskMessage, collection: MotorCollection) -> None:
+        await self.test(logger, collection)
         
-    async def getflag(self, logger: LoggerAdapter, task: CheckerTaskMessage) -> None:
-        await self.test(logger)
+    async def getflag(self, logger: LoggerAdapter, task: CheckerTaskMessage, collection: MotorCollection) -> None:
+        await self.test(logger, collection)
         
-    async def putnoise(self, logger: LoggerAdapter, task: CheckerTaskMessage) -> None:
-        await self.test(logger)
+    async def putnoise(self, logger: LoggerAdapter, task: CheckerTaskMessage, collection: MotorCollection) -> None:
+        await self.test(logger, collection)
 
-    async def getnoise(self, logger: LoggerAdapter, task: CheckerTaskMessage) -> None:
-        await self.test(logger)
+    async def getnoise(self, logger: LoggerAdapter, task: CheckerTaskMessage, collection: MotorCollection) -> None:
+        await self.test(logger, collection)
 
-    async def havoc(self, logger: LoggerAdapter, task: CheckerTaskMessage) -> None:
+    async def havoc(self, logger: LoggerAdapter, task: CheckerTaskMessage, collection: MotorCollection) -> None:
         pass
 
-    async def exploit(self, logger: LoggerAdapter, task: CheckerTaskMessage) -> None:
+    async def exploit(self, logger: LoggerAdapter, task: CheckerTaskMessage, collection: MotorCollection) -> None:
         pass
 
 
 logger = logging.getLogger()
 handler = logging.StreamHandler(sys.stdout)
-handler.setFormatter(ELKFormatter("%(message)s"))
+#handler.setFormatter(ELKFormatter("%(message)s")) ELK-ready output
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
-app = create_app(TestChecker())
+app = create_app(TestChecker(), "mongodb://127.0.0.1:27017")
+#app = create_app(TestChecker()) mongodb://mongodb:27017
