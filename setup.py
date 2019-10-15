65315921d8dd17b8fdc9ef62f52d0197a62b94d1
Benedikt Radtke <Trolldemorted@users.noreply.github.com>
2019-10-15 18:09:24 +0200

    Bump version to 0.0.9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as f:
 
 setuptools.setup(
     name="enochecker_async",
-    version="0.0.8",
+    version="0.0.9",
     author="Trolldemorted",
     author_email="benediktradtke@gmail.com",
     description="Library to build async checker scripts for the EnoEngine A/D CTF Framework in Python",
